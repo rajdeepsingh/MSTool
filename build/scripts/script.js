81401f8600391ad3a6cb6bc2bb9e8cf3913bec4f
rajdeepsingh <singhinthehouse@gmail.com>
2017-08-20 12:49:06 -0500

    initial commit

---
+++ build/scripts/script.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName('body').innderHTML = "This is a second test for the sass processing because it doesn't seem to be working for some reason";
+
+
+
+
+
+
+document.getElementsByTagName('body')[0].style.color = "#757575";
