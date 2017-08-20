95f2775b6af9e8f988695d5d564cd28899ea008b
rajdeepsingh <singhinthehouse@gmail.com>
2017-08-20 14:31:40 -0500

    added sass essentials

--- build/scripts/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementsByTagName('body').innderHTML = "This is a second test for the sass processing because it doesn't seem to be working for some reason";
-
-
-
-
-
-
-document.getElementsByTagName('body')[0].style.color = "#757575";
